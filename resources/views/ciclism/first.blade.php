324e292230a0bde6c73003e92cfe9ac9839adb35
Orosz <oroszandrei@gmail.com>
2016-03-28 13:25:46 +0300

    Ciclism Page - slider

--- resources/views/ciclism/first.blade.php
+++ resources/views/ciclism/first.blade.php
@@ -6,10 +6,112 @@
     </head>
 
         <div class="container text-center">
-            <div class="jumbotron">
-            <h1>Online Store</h1>
-            <p>Mission, Vission & Values</p>
+            <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                <!-- Indicators -->
+                <ol class="carousel-indicators">
+                    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                    <li data-target="#myCarousel" data-slide-to="1"></li>
+                    <li data-target="#myCarousel" data-slide-to="2"></li>
+                    <li data-target="#myCarousel" data-slide-to="3"></li>
+                    <li data-target="#myCarousel" data-slide-to="4"></li>
+                    <li data-target="#myCarousel" data-slide-to="5"></li>
+                    <li data-target="#myCarousel" data-slide-to="6"></li>
+
+                </ol>
+
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner" role="listbox">
+                    <div class="item active">
+                        <a href='ciclism'>  <img src="{{ asset('/img/ciclism.jpg') }}" alt="New York" width="1200" height="700">
+                            <div class="carousel-caption">
+                                <h3>Ciclism Products</h3>
+                                <p>We love ciclism !</p>
+                            </div></a>
+                    </div>
+
+                    <div class="item">
+                        <a href='ciclism'> <img src="{{ asset('/img/ciclism/slider/2.jpg') }}" alt="Chicago" width="1200" height="700">
+                            <div class="carousel-caption">
+                                <h3>Ciclism Products</h3>
+                                <p>We love ciclism !</p>
+                            </div></a>
+                    </div>
+
+                    <div class="item">
+                        <a href='ciclism'> <img src="{{ asset('/img/ciclism/slider/3.jpg') }}" alt="Chicago" width="1200" height="700">
+                            <div class="carousel-caption">
+                                <h3>Ciclism Products</h3>
+                                <p>We love ciclism !</p>
+                            </div></a>
+                    </div>
+
+                    <div class="item">
+                        <a href='ciclism'> <img src="{{ asset('/img/ciclism/slider/4.jpg') }}" alt="Chicago" width="1200" height="700">
+                            <div class="carousel-caption">
+                                <h3>Ciclism Products</h3>
+                                <p>We love ciclism !</p>
+                            </div></a>
+                    </div>
+
+                    <div class="item">
+                        <a href='ciclism'> <img src="{{ asset('/img/ciclism/slider/5.jpg') }}" alt="Chicago" width="1200" height="700">
+                            <div class="carousel-caption">
+                                <h3>Ciclism Products</h3>
+                                <p>We love ciclism !</p>
+                            </div></a>
+                    </div>
+
+                    <div class="item">
+                        <a href='ciclism'> <img src="{{ asset('/img/ciclism/slider/6.jpg') }}" alt="Chicago" width="1200" height="700">
+                            <div class="carousel-caption">
+                                <h3>Ciclism Products</h3>
+                                <p>We love ciclism !</p>
+                            </div></a>
+                    </div>
+
+
+                </div>
+
+                <!-- Left and right controls -->
+                <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+            
+           <script>
+               $(document).ready(function(){
+                   // Initialize Tooltip
+                   $('[data-toggle="tooltip"]').tooltip();
+
+                   // Add smooth scrolling to all links in navbar + footer link
+                   $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
+
+                       // Prevent default anchor click behavior
+                       event.preventDefault();
+
+                       // Store hash
+                       var hash = this.hash;
+
+                       // Using jQuery's animate() method to add smooth page scroll
+                       // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+                       $('html, body').animate({
+                           scrollTop: $(hash).offset().top
+                       }, 900, function(){
+
+                           // Add hash (#) to URL when done scrolling (default click behavior)
+                           window.location.hash = hash;
+                       });
+                   });
+               })
+           </script>
         </div>
+            <br/>
+
     </div>
 <!--
     <nav class="navbar navbar-inverse">
