cfac9bb1aae45ef9cd85ff40527ecfb532600159
Orosz <oroszandrei@gmail.com>
2016-03-30 13:28:19 +0300

    Javascript Validation Form

--- resources/views/layouts/appciclism.blade.php
+++ resources/views/layouts/appciclism.blade.php
@@ -15,6 +15,8 @@
     <!-- Styles -->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
     {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
+    <!-- JavaScripts -->
+    <script src="gen_validatorv4.js" type="text/javascript"></script>
 
     <style>
         body {
